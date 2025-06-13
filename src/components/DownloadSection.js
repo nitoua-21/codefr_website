18dfba62e8bcfdb21911a841dba3ec0bd187e9f3
Normich ITOUA <nitoua.dev@gmail.com>
2025-06-13 10:23:54 +0100

    Add Youtube video link

--- src/components/DownloadSection.js
+++ src/components/DownloadSection.js
@@ -29,6 +29,34 @@ const DownloadSection = () => {
           </div>
         </div>
         
+        {/* Video Presentation Section */}
+        <div className="columns is-centered mt-6">
+          <div className="column is-8">
+            <h3 className="title is-4 has-text-centered mb-4" style={{ color: '#00458b' }}>
+              <span className="icon mr-2">
+                <i className="fab fa-youtube"></i>
+              </span>
+              Présentation du Projet
+            </h3>
+            <div className="video-container has-text-centered">
+              <div className="video-wrapper" style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden', maxWidth: '100%', marginBottom: '2rem' }}>
+                <iframe 
+                  style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', borderRadius: '8px' }}
+                  src="https://www.youtube.com/embed/hLnkbUnFLQg?si=vk55Fs3kVtswCqgR" 
+                  title="Présentation du projet CodeFR" 
+                  frameBorder="0" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                  referrerPolicy="strict-origin-when-cross-origin" 
+                  allowFullScreen
+                ></iframe>
+              </div>
+              <p className="has-text-grey is-size-6">
+                Découvrez CodeFR en action dans cette présentation vidéo
+              </p>
+            </div>
+          </div>
+        </div>
+        
         <div className="box mt-6" style={{ borderColor: '#99ddff', borderWidth: '2px', borderStyle: 'solid' }}>
           <h3 className="title is-4 has-text-centered" style={{ color: '#00458b' }}>Instructions d'installation</h3>
           
